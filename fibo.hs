0f29fdd3bb39fce759f456270a71dafb5e33b5b2
Ya'akov Vainshtein <87584007+SulamYakov@users.noreply.github.com>
2021-07-24 22:42:23 +0300

    Added function for computing the square root of a number using a continued fraction method

--- fibo.hs
+++ fibo.hs
@@ -1,7 +1,14 @@
 
 confracPhi :: Int -> Double
 confracPhi p
---  |    p  < 1 = error
-  |    p == 1 = 1.5
-  | otherwise = 1 + ( 1 / confracPhi (p-1) )
+--  | p  < 1    = error
+    | p == 1    = 1.5
+    | otherwise = 1 + ( 1 / confracPhi (p-1) )
 
+confracSqRoot :: Int -> Double -> Double
+confracSqRoot p x
+--  | p  < 1    = error
+    | p == 1    = 1
+    | otherwise = 1 + ( (x-1) / ( 1 + (confracSqRoot (p-1) x) ) ) --need to clean this up somehow
+
+    
