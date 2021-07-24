949f8967f9af7e9d56a94d71a09f1ee2a7620178
Ya'akov Vainshtein <87584007+SulamYakov@users.noreply.github.com>
2021-07-24 22:54:25 +0300

    Commented purposes of functions

--- fibo.hs
+++ fibo.hs
@@ -1,14 +1,16 @@
 
+-- computes phi to a specified precision.
 confracPhi :: Int -> Double
 confracPhi p
---  | p  < 1    = error
+ -- | p  < 1    = error
     | p == 1    = 1.5
     | otherwise = 1 + ( 1 / confracPhi (p-1) )
 
-confracSqRoot :: Int -> Double -> Double
+-- computes the square root of a real number to a specified precision.
+confracSqRoot :: Int -> Double -> Double 
 confracSqRoot p x
---  | p  < 1    = error
+ -- | p  < 1    = error
     | p == 1    = 1
-    | otherwise = 1 + ( (x-1) / ( 1 + (confracSqRoot (p-1) x) ) ) --need to clean this up somehow
+    | otherwise = 1 + ( (x-1) / ( 1 + (confracSqRoot (p-1) x) ) ) --need to clean this up
 
     
