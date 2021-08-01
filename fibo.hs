31e32daba4fc880795b74191742b865962fc9526
Ya'akov Vainshtein <87584007+SulamYakov@users.noreply.github.com>
2021-08-01 17:06:58 +0300

    Added error cases for confrac functions

--- fibo.hs
+++ fibo.hs
@@ -2,14 +2,14 @@
 -- computes phi to a specified precision.
 confracPhi :: Int -> Double
 confracPhi p
- -- | p  < 1    = error
+    | p  < 1    = error "Invalid precision passed to confracPhi"
     | p == 1    = 1.5
     | otherwise = 1 + ( 1 / confracPhi (p-1) )
 
 -- computes the square root of a real number to a specified precision.
 confracSqRoot :: Int -> Double -> Double 
 confracSqRoot p x
- -- | p  < 1    = error
+    | p  < 1    = error "Invalid precision passed to confracSqRoot"
     | p == 1    = 1
     | otherwise = 1 + ( (x-1) / ( 1 + (confracSqRoot (p-1) x) ) ) --need to clean this up
 
